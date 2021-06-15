5dbaa8acc82dfdba2cd890c2d18c3f16c416e0a5
Emilia <e.lubanska@gmail.com>
2021-06-15 19:31:52 -0400

    Implementing the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // --- our code goes here ---
+    const $textarea = $("form textarea");
+    
+    $textarea.on("input", function(event) {
+        const count = $(this).val().length;
+        // console.log(count);
+        const $counter = $(this).next().find(".counter");
+        // console.log($counter);
+        $counter.text(140 - count);
+        // console.log(counter.text);
+        if (count > 140) {
+            $counter.css("color", "red");
+          };
+    });
+  });
+//   $("#btn").on('click', function() {
+//     console.log(this); //The this keyword is a reference to the button
+//   });
+  
+//   $("#btn").on('click', () => {
+//     console.log(this); //The this keyword here refers to something else!
+//   });
